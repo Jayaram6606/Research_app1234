cd44a7dd00097d8c17f719d4ad5a29b1d9b4c821
Jayaram Dev <jayaram@gmail.com>
2026-02-26 15:49:58 +0530

    Add prompt template for summarizing research papers and update UI to include length input

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,6 +1,8 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core import PromptTemplate, load_prompt
+
 
 load_dotenv()
 
@@ -13,6 +15,11 @@ paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You
 style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+
+
+template = load_prompt('template.json')
+
 
 if st.button("Submit"):
     st.write("hello")
